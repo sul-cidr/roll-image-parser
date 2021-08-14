c13b24b1ae24b714560dd54555a1d3a2f4515340
Peter Broadwell <broadwell@stanford.edu>
2021-08-13 21:23:02 -0700

    Fix arithmetic of MIDI number reassignment based on rewind hole position
    Previously, there was a bug in the adjustment of each hole's MIDI number that
    is done when realigning the tracker columns and MIDI numbers based on an
    observed discrepancy between the detected rewind hole's MIDI number and the
    MIDI number the rewind hole should have given the roll type. The code shifted
    the MIDI numbers in the wrong direction, i.e., it interpreted the sign of the
    value of `shifting` incorrectly. This value should be *subtracted* from the
    MIDI number of each hole to correct the holes' alignment with the virtual
    tracker bar, and conversely it should be *added* to the value of the mapping
    from MIDI numbers to tracker bar columns (this part was already being done
    correctly).

--- src/RollImage.cpp
+++ src/RollImage.cpp
@@ -576,18 +576,34 @@ void RollImage::assignMidiKeyNumbersToHoles(void) {
 		return;
 	}
 
+	// this is the difference between the MIDI number currently assigned to the
+	// detected rewind hole's tracker column and the MIDI number that the
+	// rewind hole's tracker column should have for this roll type,
+	// assuming that the detected rewind hole truly is the rewind hole
 	int shifting = midiKey[bestFitIndex] - rewindholemidi;
 	cerr << "SHIFTING HOLE ASSIGNMENTS BY " << shifting << " FOR REWIND HOLE ALIGNMENT" << endl;
 
 	for (int i=0; i<(int)trackerArray.size(); i++) {
 		for (int j=0; j<(int)trackerArray[i].size(); j++) {
+			// adjust the MIDI number of every hole in every column by the
+			// discrepancy between the observed and expected MIDI number of the
+			// rewind hole. Holes in tracker columns with midikey < 0 have
+			// already been marked as invalid and won't be affected.
 			if (trackerArray[i][j]->midikey >= 0) {
-				trackerArray[i][j]->midikey += shifting;
+				trackerArray[i][j]->midikey -= shifting;
 			}
+			// each hole's tracker column number is adjusted in the opposite
+			// direction as the MIDI number, to keep it in sync with the
+			// midiToTrackMapping (see below)
 			trackerArray[i][j]->track += shifting;
 		}
 	}
 
+	// adjust the tracker column mapping for each MIDI number by the observed
+	// discrepancy between the current and expected MIDI number of the rewind
+	// hole. This keeps midiToTrackMapping in sync with the adjusted MIDI
+	// number mappings for the holes (above), and is made in the opposite
+	// direction for this reason.
 	for (int i=0; i<(int)midiToTrackMapping.size(); i++) {
 		if (midiToTrackMapping[i] > 0) {
 			midiToTrackMapping[i] += shifting;
