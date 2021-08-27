0ee95a35350092969424de579c80cab25003a00c
Simon Wiles <simonjwiles@gmail.com>
2021-08-27 11:35:43 -0700

    Consistency in TPQ formats

--- src/RollImage.cpp
+++ src/RollImage.cpp
@@ -4252,12 +4252,12 @@ void RollImage::setMidiFileTempo(MidiFile& midifile) {
 	if (m_rollType == "welte-red") {
 		// TPQ is 6 times the tempo at 300 DPI, (so 591 = 6 * 98.5)
 		// 591 is for Red Welte tempo of 98.5 (~3 meters/minute).
-		midifile.setTPQ(591);
+		midifile.setTPQ(6 * 98.5);
 	} else if (m_rollType == "welte-green") {
 		// Peter Phillips's dissertation describes the speed for green Welte
 		// rolls as "seven feet per minute" (p. 121). 7ft/min = 2.1336m/min =
 		// tempo of 70.0532 (* 6 = 420)
-		midifile.setTPQ(420);
+		midifile.setTPQ(6 * 70);
 	} else if (m_rollType == "88-note") {
 		midifile.setTPQ(6 * 60); 
 	} else {
